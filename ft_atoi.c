9001b79b9d82e381fd29f363296077e2e45accd7
Jhonatan Ojeda Martucci <jhonatan@MacBook-Air-de-Jhonatan.local>
2024-04-04 18:29:39 +0200

    ft_atoi Paco appruved

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jojeda-m <jojeda-m@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:08:01 by jojeda-m          #+#    #+#             */
-/*   Updated: 2024/04/04 14:21:41 by jojeda-m         ###   ########.fr       */
+/*   Updated: 2024/04/04 18:26:42 by jojeda-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,26 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	nb;
-	int	negativenb;
+	int	nbr;
+	int	sign;
 
-	i = 0;
-	nb = 0;
-	negativenb = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	while (str[i] == '-' || str[i] == '+')
-		if (str[i++] == '-')
-			negativenb++;
-	while (str[i] >= '0' && str[i] <= '9')
-		nb = nb * 10 + (str[i++] - '0');
-	if (negativenb % 2 == 1)
-		return (nb * -1);
-	return (nb);
+	nbr = 0;
+	sign = 1;
+	while ((*str == ' ') || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str++ == '-')
+			sign = -1;
+	}
+	while (*str != '\0')
+	{
+		if (!ft_isdigit(*str))
+			return (nbr * sign);
+		nbr = (nbr * 10) + (*str - '0');
+		str++;
+	}
+	return (nbr * sign);
 }
 
 /*int	main(void) 
